2a5192d09267d129b3c175d53e3d428026078c2a
duanjinfu <755431761@qq.com>
2022-03-29 17:21:22 +0800

    111

--- code/4.go-sql-driver/1_test.go
+++ code/4.go-sql-driver/1_test.go
@@ -3,18 +3,18 @@ package sdf
 import (
 	"database/sql"
 	"fmt"
-	"testing"
 	_ "github.com/go-sql-driver/mysql"
+	"testing"
 )
 
 var (
 	db *sql.DB
-	err error
 )
+
 type Wp_usermeta struct {
-	Umeta_id int `json:"umeta_id"`
-	User_id int `json:"user_id"`
-	Meta_key string `json:"meta_key"`
+	Umeta_id   int    `json:"umeta_id"`
+	User_id    int    `json:"user_id"`
+	Meta_key   string `json:"meta_key"`
 	Meta_value string `json:"meta_value"`
 }
 
@@ -28,12 +28,13 @@ func init() {
 	db, err = sql.Open("mysql", "root:fstxlab@2017!@tcp(123.57.174.21:3306)/horizon?charset=utf8")
 	painc_err(err)
 }
+
 //查询单条记录
 func TestName(t *testing.T) {
 	stmt, err := db.Prepare("select * from wp_usermeta where umeta_id = ?")
-	defer stmt.Close()  
+	defer stmt.Close()
 	painc_err(err)
-	row:= stmt.QueryRow(12)
+	row := stmt.QueryRow(12)
 	// 以上为使用预处理方式 查询 。也可以使用非预处理方式查询
 	//row := db.QueryRow("select * from wp_usermeta where umeta_id = 12")
 	var usermeta Wp_usermeta
@@ -41,6 +42,7 @@ func TestName(t *testing.T) {
 	painc_err(err)
 	fmt.Println(usermeta)
 }
+
 //查询多条记录
 func TestName1(t *testing.T) {
 	stmt, err := db.Prepare("select * from wp_usermeta where umeta_id < ?")
@@ -58,22 +60,25 @@ func TestName1(t *testing.T) {
 		fmt.Println(usermeta)
 	}
 }
+
 //	写入 更新 删除数据 一致 只是sql 不一样
 func TestName3(t *testing.T) {
 	stmt, err := db.Prepare("insert into wp_usermeta(user_id,meta_key,meta_value) value (?,?,?)")
 	painc_err(err)
 	defer stmt.Close()
-	res, err := stmt.Exec(2,"test3","test4")
+	res, err := stmt.Exec(2, "test3", "test4")
 	painc_err(err)
 	row_num, err := res.RowsAffected() // 受影响的行数
 	painc_err(err)
-	id,err := res.LastInsertId()	// 最后写入的id
+	id, err := res.LastInsertId() // 最后写入的id
 	painc_err(err)
-	fmt.Println(row_num,id)
+	fmt.Println(row_num, id)
 }
+
 // 事务
 func Test44(t *testing.T) {
 	tx, err := db.Begin()
+	// 对数据的所有操作要用tx进行操作 tx.query()... tx.queryrow ...
 	err = tx.Commit()
 	err = tx.Rollback()
 	fmt.Println(err)
--- code/4.go-sql-driver/mysql.go
+++ code/4.go-sql-driver/mysql.go
@@ -3,26 +3,32 @@ package sdf
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 // go-sql-driver 最大的问题  scan 是按照顺序 解析的  一旦数据库结构改变  将会全部错乱  代码直接报错
+//  不封装mysql 的事务   query 相关的stmt必须在commit 或者rollback之后才能关闭  封装并不能defer close 意义不大  用原生db写吧
+
 var Db *sql.DB
+var err error
 
 func init() {
-	database, err := sql.Open("mysql", "root:123456@tcp(42.194.133.108:3306)/bbs")
+	Db, err = sql.Open("mysql", "root:123456@tcp(42.194.133.108:3306)/bbs")
+	Db.SetMaxOpenConns(99)                                 // 最大连接数
+	Db.SetMaxIdleConns(90)                                 //最大空闲连接数
+	Db.SetConnMaxLifetime(time.Duration(time.Second * 60)) // 连接超时世间
+	Db.SetConnMaxIdleTime(time.Duration(time.Second * 60)) // 连接空闲持续时间
+
 	//checkErr(err)
-	if err != nil {
-		panic("连接数据库错误")
-	}
-	Db = database
 }
 
 func Exec(sqlString string, parames ...interface{}) (sql.Result, error) {
+	var stmt *sql.Stmt
 	stmt, err := Db.Prepare(sqlString)
 	if err != nil {
 		return nil, err
 	}
-	defer stmt.Close()
+	defer stmt.Close() //busy buffer  bad connection 错误的始作俑者  必须在tx.commit 之后再关闭
 	res, err := stmt.Exec(parames...)
 	// 判断修改数据 成功失败 应该以err 是不是为空 为判断依据， 返回res 用于获取其他数据
 	return res, err
@@ -34,12 +40,12 @@ func Query(sqlString string, limit int, page int, parames ...interface{}) (*sql.
 	if err != nil {
 		return nil, err
 	}
-	defer stmt.Close()
-	var rows *sql.Rows
+	defer stmt.Close() //busy buffer  bad connection 错误的始作俑者  必须在tx.commit 之后再关闭
+
 	if limit != 0 && page != 0 {
 		parames = append(parames, (page-1)*limit, limit)
 	}
-	rows, err = stmt.Query(parames...)
+	rows, err := stmt.Query(parames...)
 	if err != nil {
 		panic(err)
 	}
@@ -48,6 +54,9 @@ func Query(sqlString string, limit int, page int, parames ...interface{}) (*sql.
 
 func QueryNoParame(sqlString string) (*sql.Rows, error) {
 	rows, err := Db.Query(sqlString)
+	if err != nil {
+		return nil, err
+	}
 	//defer rows.Close()   函数调用方还需要用到该资源 此处不能关闭，不然外面获取不到值  所以必须在调用处 defer close
 	return rows, err
 }
@@ -59,7 +68,6 @@ func QueryRow(sqlString string, parames ...interface{}) (*sql.Row, error) {
 		return nil, err
 	}
 	defer stmt.Close()
-	var row *sql.Row
-	row = stmt.QueryRow(parames...)
-	return row, err
+	row := stmt.QueryRow(parames...)
+	return row, nil
 }
