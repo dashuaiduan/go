b481d1ceeed5ac37c1ce74571fba1794fbd4d454
duanjinfu <755431761@qq.com>
2022-04-25 10:51:20 +0800

    context

--- Basics/7_test.go
+++ Basics/7_test.go
@@ -1,6 +1,7 @@
 package test1
 
 import (
+	"context"
 	"fmt"
 	"testing"
 	"time"
@@ -25,4 +26,23 @@ func Test71(t *testing.T) {
 		time.Sleep(2 * time.Second)
 
 	}
+}
+func Test72(t *testing.T) {
+	d := time.Now().Add(10 * time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
+	// 尽管ctx会过期，但在任何情况下调用它的cancel函数都是很好的实践。
+	// 如果不这样做，可能会使上下文及其父类存活的时间超过必要的时间。
+	defer cancel()
+
+	for  {
+		select {
+		case <-time.After(1 * time.Second):
+			fmt.Println("overslept")
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		}
+	}
+
+
 }
