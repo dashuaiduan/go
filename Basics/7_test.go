fe906144d4d45e8725e3d8624f8582c6a867285e
duanjinfu <755431761@qq.com>
2022-04-29 18:12:37 +0800

    context

--- Basics/7_test.go
+++ Basics/7_test.go
@@ -34,7 +34,7 @@ func Test72(t *testing.T) {
 	// 如果不这样做，可能会使上下文及其父类存活的时间超过必要的时间。
 	defer cancel()
 
-	for  {
+	for {
 		select {
 		case <-time.After(1 * time.Second):
 			fmt.Println("overslept")
@@ -44,5 +44,64 @@ func Test72(t *testing.T) {
 		}
 	}
 
+}
 
+type AA interface {
+	a1(string) string
+}
+type BB struct {
+}
+
+func (b BB) a1(string2 string) string {
+	return "55"
+}
+func work() {
+	defer fmt.Println("defer work")
+	panic("666")
+	time.Sleep(3 * time.Second)
+	fmt.Println("work over!")
+}
+func recoverName() {
+	if r := recover(); r != nil {
+		fmt.Println("recovered from ", r)
+	}
+}
+func to_work() {
+	defer fmt.Println("defer to_work")
+	go work()
+}
+func Test73(t *testing.T) {
+	defer recoverName()
+	defer fmt.Println("main defer")
+	to_work()
+	time.Sleep(5 * time.Second)
+	fmt.Println("main over")
+}
+
+func fullName(firstName *string, lastName *string) {
+	defer fmt.Println("deferred call in fullName")
+	if firstName == nil {
+		panic("runtime error: first name cannot be nil")
+	}
+	if lastName == nil {
+		panic("runtime error: last name cannot be nil")
+	}
+	fmt.Printf("%s %s\n", *firstName, *lastName)
+	fmt.Println("returned normally from fullName")
+}
+
+func Test74(t *testing.T) {
+	defer fmt.Println("deferred call in main")
+	firstName := "Elon"
+	fullName(&firstName, nil)
+	fmt.Println("returned normally from main")
+}
+func Test75(t *testing.T) {
+	a := make([]int, 0, 6)
+	a = append(a, 1, 2, 3)
+	b := []int{11, 22, 33}
+	copy(a[len(a):], b)
+	fmt.Println(a)
+	zz := a[1:]
+	fmt.Println(zz)
 }
