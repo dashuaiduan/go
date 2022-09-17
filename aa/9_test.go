b8042e11e8d16fd16b4ef73ffa62ce0ed03363dc
duanjinfu <755431761@qq.com>
2022-09-17 14:12:51 +0800

    1111

--- aa/9_test.go
+++ aa/9_test.go
@@ -3,9 +3,22 @@ package aa
 import (
 	"a/utils"
 	"fmt"
+	"sync"
 	"testing"
 )
 
 func TestName111(t *testing.T) {
-	fmt.Println(utils.GetHourDiffer("2022-09-01 04:00:00", "2022-09-02 08:00:00"))
+	fmt.Println(utils.GetHourDiffer("2022/09/01 04:00:00", "2022/09/02 08:00:00"))
+
+	m := map[string]string{"dsgf": "11", "dd": "555"}
+	fmt.Println(m["356"])
+}
+
+func Test333(t *testing.T) {
+	var m sync.Map
+	m.Store(1, 111)
+	m.Store(1, 113)
+	m.Delete(1)
+	fmt.Println(m.Load(1))
+	fmt.Println(m)
 }
