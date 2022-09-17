6ad92475868d17bc7edd056dd52beed15b6aabff
755431761@qq.com <755431761@qq.com>
2022-09-17 23:47:06 +0800

    grpc 安全认证 over

--- code/grpc/service/product_service.go
+++ code/grpc/service/product_service.go
@@ -2,10 +2,13 @@ package service
 
 import "context"
 
-var ProductServiceV ProductService
+var ProductServiceV = &productService{}
 
-type ProductService struct{}
-
-func (p ProductService) GetProductStock(ctx context.Context, request *ProductRequest) (*ProductResponse, error) {
-	return &ProductResponse{ProdStock: request.ProdId + 100}, nil
+//直接找pb.go 文件中的接口  复制过来实现两个方法即可
+type productService struct {
 }
+
+func (p *productService) GetProductStock(ctx context.Context, request *ProductRequest) (*ProductResponse, error) {
+	return &ProductResponse{ProdStock: request.ProdId}, nil
+}
+func (p *productService) mustEmbedUnimplementedProdServiceServer() {}
